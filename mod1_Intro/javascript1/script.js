bb8fb549573ed5ae6302a4bc52207f30412817d6
Lorenzo-Giacomo <lorenzogiacomo1@gmail.com>
2022-04-04 21:04:37 -0300

    first commit

---
+++ mod1_Intro/javascript1/script.js
@@ -0,0 +1,28 @@
+// AULA DE FORMAS DE MOSTRAR O OUTPUT
+// 4 formas principais de mostrar informações para os usuários finais:
+// 1 colocar id no elemento, selecionar e trocar o html:
+// document.getElementById('titulo').innerHTML = 'Opa, tudo bem?'
+
+// 2 você quase nunca irá usar. Criando o seu próprio elemento.
+// document.write('Algum texto')
+
+// 3 Usando a janela do navegador. Página: alterações em todo conteúdo, documento 'document.' acima. Janela do navegador: não tem muito relação com o documento, e sim com o que está sendo visto.
+// window.alert('Mensagem de exemplo')
+
+// 4 Console é possível ver erros, mensagens, etc. Pode ser para especificar algo para o desenvolvedor.
+// console.log('A mensagem que eu inserir aqui, irá aparecer no console. ')
+
+//*-************************************************************************
+
+// AULA VARIÁVEIS
+// Principal forma de criar variáveis:
+// var nome = 'Lorenzo Giacomo'
+// var idade = 90
+// window.alert(`${nome} tem ${idade} anos de idade`)
+
+// é possível somar variáveis e salvar em outra variáveis. Com strings é concatenado.
+
+// *************************************************************************
+
+// AULA CONDICIONAIS
+// "caixinha de código"
